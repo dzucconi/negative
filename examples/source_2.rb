ecaf777b6ab58e6f16d21cf0d375ecfc57c78aef
dzucconi <mail@damonzucconi.com>
2013-07-29 19:06:08 -0400

    Remove whitespace

--- examples/source_2.rb
+++ examples/source_2.rb
@@ -1,1 +1,1 @@
-puts (1..100).map{ |b| c = [b % 3 < 1 ? :fizz : "", b % 5 < 1 ? :buzz : ""] * ""; c == "" ? b : c }.join("\n")
+puts (1..100).map{|b|c=[b%3<1?:fizz:"",b%5<1?:buzz:""]*"";c==""?b:c}.join("\n")
